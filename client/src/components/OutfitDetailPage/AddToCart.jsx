c43fc32a11704eba610e0178a46ad2005ccc0fd9
Shailee Patel <shaileeee@gmail.com>
2020-12-30 02:43:03 -0800

    remove modal pop up for sold out outfits and refactor component

---
+++ client/src/components/OutfitDetailPage/AddToCart.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from 'styled-components';
+
+var AddToCart = (props) => {
+  return (
+    <div>
+      <button className={props.outfitInfo.availability ? "btn btn-dark" : "btn btn-secondary"} data-toggle="modal" data-target="#addToBagModal">
+        {props.outfitInfo.availability ? "Add To Cart" : "Sold Out"}
+      </button>
+      {props.outfitInfo.availability ?
+        <div className="modal fade" id="addToBagModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+          <div className="modal-dialog modal-dialog-centered" role="document">
+            <Div className="modal-content">
+              <div className="modal-header">
+                <H6 className="modal-title" id="exampleModalLongTitle">You added {props.outfitInfo.name} to your shopping cart</H6>
+                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+            </Div>
+          </div>
+        </div>
+      : null}
+    </div>
+  )
+}
+
+var Div = styled.div`
+  background-color: #e5efe5;
+`
+
+var H6 = styled.div`
+  color: #006400;
+`
+
+export default AddToCart;
--- client/src/components/OutfitDetailPage/index.jsx
+++ client/src/components/OutfitDetailPage/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-
+import AddToCart from './AddToCart.jsx';
 
 var OutfitDetailPage = (props) => {
   return (
@@ -17,22 +17,7 @@ var OutfitDetailPage = (props) => {
           <div className="h6 mt-3">Size: {props.outfitInfo.size.toUpperCase()}</div>
           <div className="mt-3">{props.outfitInfo.description}.</div>
           <div className="mt-4">
-            <button className={props.outfitInfo.availability ? "btn btn-dark" : "btn btn-secondary"} data-toggle="modal" data-target="#addToBagModal">
-              {props.outfitInfo.availability ? "Available" : "Sold Out"}
-            </button>
-            {/* Modal */}
-            <div className="modal fade" id="addToBagModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-              <div className="modal-dialog modal-dialog-centered" role="document">
-                <Div className="modal-content">
-                  <div className="modal-header">
-                    <H6 className="modal-title" id="exampleModalLongTitle">You added {props.outfitInfo.name} to your shopping cart</H6>
-                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                      <span aria-hidden="true">&times;</span>
-                    </button>
-                  </div>
-                </Div>
-              </div>
-            </div>
+            <AddToCart outfitInfo = {props.outfitInfo}/>
           </div>
           <Button className="mt-2">
             <Svg onClick = {props.goBackFromOutfitDetail} viewBox="0 0 18 18" role="presentation" aria-hidden="true" focusable="false"><path d="m13.7 16.29a1 1 0 1 1 -1.42 1.41l-8-8a1 1 0 0 1 0-1.41l8-8a1 1 0 1 1 1.42 1.41l-7.29 7.29z" fillRule="evenodd"></path></Svg>
@@ -65,13 +50,5 @@ var Button = styled.button`
     background-color: rgba(34, 34, 34, 0.04);
   }
 `;
-
-var Div = styled.div`
-  background-color: #e5efe5;
-`
-
-var H6 = styled.div`
-  color: #006400;
-`
 
 export default OutfitDetailPage;
