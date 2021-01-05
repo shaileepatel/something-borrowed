e8d399c36755321911face7aa811842b0ad039aa
Shailee Patel <shaileeee@gmail.com>
2021-01-04 17:38:02 -0800

    make the filter componenet and the outfit details component sticky

--- client/src/components/OutfitDetailPage/index.jsx
+++ client/src/components/OutfitDetailPage/index.jsx
@@ -12,16 +12,18 @@ var OutfitDetailPage = (props) => {
           )}
         </div>
         <div className="col-6">
-          <div className="h3">{props.outfitInfo.name}</div>
-          <div className="h6 font-weight-bold mt-3">${props.outfitInfo.price}</div>
-          <div className="h6 mt-3">Size: {props.outfitInfo.size.toUpperCase()}</div>
-          <div className="mt-3">{props.outfitInfo.description}.</div>
-          <div className="mt-4">
-            <AddToCart outfitInfo = {props.outfitInfo}/>
+          <div className="sticky-top">
+            <div className="h3">{props.outfitInfo.name}</div>
+            <div className="h6 font-weight-bold mt-3">${props.outfitInfo.price}</div>
+            <div className="h6 mt-3">Size: {props.outfitInfo.size.toUpperCase()}</div>
+            <div className="mt-3">{props.outfitInfo.description}.</div>
+            <div className="mt-4">
+              <AddToCart outfitInfo = {props.outfitInfo}/>
+            </div>
+            <Button className="mt-2">
+              <Svg onClick = {props.goBackFromOutfitDetail} viewBox="0 0 18 18" role="presentation" aria-hidden="true" focusable="false"><path d="m13.7 16.29a1 1 0 1 1 -1.42 1.41l-8-8a1 1 0 0 1 0-1.41l8-8a1 1 0 1 1 1.42 1.41l-7.29 7.29z" fillRule="evenodd"></path></Svg>
+            </Button>
           </div>
-          <Button className="mt-2">
-            <Svg onClick = {props.goBackFromOutfitDetail} viewBox="0 0 18 18" role="presentation" aria-hidden="true" focusable="false"><path d="m13.7 16.29a1 1 0 1 1 -1.42 1.41l-8-8a1 1 0 0 1 0-1.41l8-8a1 1 0 1 1 1.42 1.41l-7.29 7.29z" fillRule="evenodd"></path></Svg>
-          </Button>
         </div>
       </div>
     </div>
